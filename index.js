f41892598b720a73097e7ab0f0d511e351da3cc2
Jose Luis Jardon <jljardon.c@gmail.com>
2017-12-08 02:21:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
+
 function getFirstSelector(selector){
-   return document.querySelector(selector);
-}
-
-function nestedTarget(){
-   return document.getElementById('app').querySelector('#nested');
-}
-
-function deepestChild(){
-   return document.getElementById('grand-node').querySelector('div div div div div');
-}
-
-function increaseRankBy(n){
-   function getFirstSelector(selector){
    return document.querySelector(selector);
 }
 
