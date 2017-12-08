6e9a22cf370c92eee92e104e91cf6c4a2777abd8
Jose Luis Jardon <jljardon.c@gmail.com>
2017-12-08 02:23:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function increaseRankBy(n){
    const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
 
    for (let i = 0; i < lis.length; i++) {
-     lis[i].innerHTML = (i + n).toString()
+     lis[i].innerHTML = (i + n + 1).toString()
    }
 
 }
