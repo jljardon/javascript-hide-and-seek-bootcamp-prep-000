476035c813069776c66c074036ea36888c4b7284
Jose Luis Jardon <jljardon.c@gmail.com>
2017-12-08 02:51:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function getFirstSelector(selector){
+   return document.querySelector(selector);
+}
+
+function nestedTarget(){
+   return document.getElementById('app').querySelector('#nested');
+}
+
+function deepestChild(){
+   return document.getElementById('grand-node').querySelector('div div div div div');
+}
+
+function increaseRankBy(n){
+   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+   for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+   }
+}
