0438f1e6c69354e6174f6e72a48429bacf2dda12
Jose Luis Jardon <jljardon.c@gmail.com>
2017-12-08 02:21:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ function nestedTarget(){
 function deepestChild(){
    return document.getElementById('grand-node').querySelector('div div div div div');
 }
+
+function increaseRankBy(n){
+
+   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+    
+   for (let i = 0; i < lis.length; i++) {
+     lis[i].innerHTML = (i + 4).toString()
+   }
+   
+}
